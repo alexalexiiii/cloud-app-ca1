f9c3300db836e13a3230ce9d687f1e0a364f1982
alexalexiiii <20101924@mail.wit.ie>
2025-10-22 11:05:04 +0100

    debugged errors

--- movies-app/lib/movies-app-stack.ts
+++ movies-app/lib/movies-app-stack.ts
@@ -1,16 +1,41 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { RemovalPolicy } from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import * as cognito from 'aws-cdk-lib/aws-cognito';
+import * as eventSources from 'aws-cdk-lib/aws-lambda-event-sources';
+
 
 export class MoviesAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+   const table = new dynamodb.Table(this, 'SingleTable', {
+    partitionKey: {name: 'PK', type: dynamodb.AttributeType.STRING },
+    billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    removalPolicy: RemovalPolicy.DESTROY,
+    stream: dynamodb.StreamViewType.NEW_AND_OLD_IMAGES
+   });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MoviesAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+   const apiHandler = new lambda.Function(this, 'ApiHandler', {
+    runtime: lambda.Runtime.NODEJS_18_X,
+    code: lambda.Code.fromAsset('lambda/api'),
+    handler: 'index.handler',
+    environment: { TABLE_NAME: table.tableName}
+   });
+   table.grantReadWriteData(apiHandler);
+
+   const stateLogger = new lambda.Function(this, 'StateLogger', {
+    runtime: lambda.Runtime.NODEJS_18_X,
+    code: lambda.Code.fromAsset('lambda/stateLogger'),
+    handler: 'index.handler'
+   });
+   stateLogger.addEventSource(new eventSources.DynamoEventSource(table, {
+    startingPosition: lambda.StartingPosition.TRIM_HORIZON,
+    batchSize: 5
+   }));
+
   }
 }
