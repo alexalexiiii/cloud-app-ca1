937186fc36e2771483b0704d943aa0e5dc256ad7
alexalexiiii <20101924@mail.wit.ie>
2025-10-27 11:24:05 +0000

    added state logger lambda

--- movies-app/lib/movies-app-stack.ts
+++ movies-app/lib/movies-app-stack.ts
@@ -1,3 +1,8 @@
+/**
+ * references for function structure
+ * https://cdkworkshop.com
+ * https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_lambda-readme.html
+ */
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { RemovalPolicy } from 'aws-cdk-lib';
@@ -36,6 +41,12 @@ export class MoviesAppStack extends cdk.Stack {
     startingPosition: lambda.StartingPosition.TRIM_HORIZON,
     batchSize: 5
    }));
+
+   const userPool = new cognito.UserPool(this, 'UserPool', { selfSignUpEnabled: true, signInAliases: { email: true } });
+   const userPoolClient = new cognito.UserPoolClient(this, 'AppClient', { userPool });
+
+   
+
 
   }
 }
