832676504312427bb947c5ed53a59e46bb628e54
alexalexiiii <20101924@mail.wit.ie>
2025-11-06 19:28:19 +0000

    added movies seed file to populate dynamodb table

--- movies-app/lib/stacks/api-stack.ts
+++ movies-app/lib/stacks/api-stack.ts
@@ -15,3 +15,19 @@ interface ApiStackProps extends StackProps {
   userPool: cognito.UserPool;
   logGroup: logs.LogGroup;
 }
+
+// handling all rest api logic for movies app, defines lambda function+api gateway routes and auth
+export class ApiStack extends Stack {
+    constructor(scope: Construct, id: string, props: ApiStackProps) {
+        super(scope, id, props);
+
+        const { table, userPool, logGroup } = props;
+
+        const lambdaRole = new iam.Role(this, 'LambdaRole', {
+            assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+            managedPolicies: [ 
+                iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole')
+            ],
+        });
+        table.grantReadWriteData(lambdaRole);
+
