56681e9c64c3ac7958dd901740ad098a271f639d
alexalexiiii <20101924@mail.wit.ie>
2025-11-09 15:43:20 +0000

    wrote get all actors in a movie function, specific actor in movie, get function for award

--- movies-app/lib/lambda/api/index.ts
+++ movies-app/lib/lambda/api/index.ts
@@ -51,3 +51,59 @@ export const handler: APIGatewayProxyHandlerV2 = async (event) => {
 
       return response(200, movieData);
     }
+
+    // ---- GET ALL ACTORS IN A MOVIE ----
+    if (method === "GET" && path.match(/^\/movies\/[0-9]+\/actors$/)) {
+      const movieId = pathParams.movieid || path.split("/")[2];
+
+      const cast = await ddb.send(
+        new QueryCommand({
+          TableName: TABLE_NAME,
+          KeyConditionExpression: "PK = :pk",
+          ExpressionAttributeValues: marshall({ ":pk": `c${movieId}` }),
+        })
+      );
+
+      return response(200, cast.Items?.map((i) => unmarshall(i)) || []);
+    }
+
+    // ---- GET SPECIFIC ACTOR IN A MOVIE ----
+    if (method === "GET" && path.match(/^\/movies\/[0-9]+\/actors\/[0-9]+$/)) {
+      const movieId = pathParams.movieid || path.split("/")[2];
+      const actorId = pathParams.actorid || path.split("/")[4];
+
+      const castMember = await ddb.send(
+        new GetItemCommand({
+          TableName: TABLE_NAME,
+          Key: marshall({ PK: `c${movieId}`, SK: actorId }),
+        })
+      );
+
+      if (!castMember.Item)
+        return response(404, { error: "Cast member not found" });
+
+      return response(200, unmarshall(castMember.Item));
+    }
+
+    // ---- GET AWARDS ----
+    if (method === "GET" && path.startsWith("/awards")) {
+      const { movie, actor, awardBody } = queryParams;
+
+      let key = "";
+      if (movie) key = `w${movie}`;
+      else if (actor) key = `w${actor}`;
+      else return response(400, { error: "movie or actor required" });
+
+      let params: any = {
+        TableName: TABLE_NAME,
+        KeyConditionExpression: "PK = :pk",
+        ExpressionAttributeValues: marshall({ ":pk": key }),
+      };
+
+      if (awardBody) params.KeyConditionExpression += " and SK = :sk", 
+      params.ExpressionAttributeValues[":sk"] = awardBody;
+
+      const awards = await ddb.send(new QueryCommand(params));
+      return response(200, awards.Items?.map((i) => unmarshall(i)) || []);
+    }
+
